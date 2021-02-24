8618ff3de24ec7cc03cfb3bb983a942d2bf40b52
25_Abdulelah Win_SEI_14 <abdulelah.o.ismail@gmail.com>
2021-02-24 17:20:02 +0300

    deployment 1

--- letslearn/settings.py
+++ letslearn/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver.nostatic',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -172,6 +172,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
